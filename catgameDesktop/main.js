2313d5439947a6524a0efbf29b2c138f1a02fe2c
88ROKAS88 <rokas8misevicius@gmail.com>
2023-02-01 18:46:44 +0200

    added menu
    added menu

--- catgameDesktop/main.js
+++ catgameDesktop/main.js
@@ -2,11 +2,47 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
 app.on("ready", function () {
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+  });
   mainWindow.loadURL(`file://${__dirname}/mainWindow.html`);
 });
+
+const mainMenuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      { label: "New Game" },
+      {
+        label: "Exit",
+        accelerator: "Ctrl+Q",
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+  {
+    label: "Developer Tools",
+    submenu: [
+      {
+        label: "Toggle Developer Tools",
+        accelerator: "Ctrl+I",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        },
+      },
+      {
+        role: "reload",
+      },
+    ],
+  },
+];
+const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+Menu.setApplicationMenu(mainMenu);
